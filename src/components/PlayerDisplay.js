619ab0fb938d97e43ebfcdc5118bc93eb45fdddd
Kate Fox <katefox100@gmail.com>
2021-03-30 15:25:25 +0100

    randomiser logic working in PlayerDisplay component

--- src/components/PlayerDisplay.js
+++ src/components/PlayerDisplay.js
@@ -1,19 +1,45 @@
-import { Component } from "react";
+const PlayerDisplay = () => {
 
-class PlayerDisplay extends Component {
+    // const [number, setNumber] = useState(1);
 
-    render() {
-        return (
-            <>
-                <div className="card card-body col-md-6 mt-4">
-                    <h4 className="card-title">The players are:</h4>
-                    <p className="card-body">Harry, Killian, Bob, Jack</p>
-                </div>
-                
-            </>
-        )
+    let array = ["Bill", "Jim", "Said", "Wilma"];
+    console.log(array);
+
+    let handleRandomise = (array) => {
+
+        for (let i = array.length - 1; i >= 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+        
+            // swap elements array[i] and array[j]
+            // we use "destructuring assignment" syntax to achieve that
+            // you'll find more details about that syntax in later chapters
+            // same can be written as:
+            // let t = array[i];array[i] = array[j]; array[j] = t;
+
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+
+        console.log(array);
+        return array;
+
     }
 
+    handleRandomise(array);
+
+    return (
+        <>
+            <div className="card card-body col-md-6 mt-4">
+                <h4 className="card-title">The contenders are:</h4>
+                <p className="card-body">A list of the inputted names</p>
+                <button
+                    className="btn btn-primary"
+                    // onClick={ handleRandomise }
+                    >Generate random pairings
+                </button>
+            </div>
+            
+        </>
+    );
 
 }
 
