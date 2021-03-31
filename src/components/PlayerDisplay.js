df917958e0afe224f5cec2952f59e9a642f5bdd6
Kate Fox <katefox100@gmail.com>
2021-03-31 10:50:45 +0100

    components now functional not classes, randomiser working on button

--- src/components/PlayerDisplay.js
+++ src/components/PlayerDisplay.js
@@ -1,39 +1,43 @@
+// import { useState } from "react";
+
 const PlayerDisplay = () => {
 
-    // const [number, setNumber] = useState(1);
+    // const [players, setPlayers] = useState([]);
 
-    let array = ["Bill", "Jim", "Said", "Wilma"];
-    console.log(array);
-
-    let handleRandomise = (array) => {
+    const randomise = (array) => {
 
         for (let i = array.length - 1; i >= 0; i--) {
             let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
-        
             // swap elements array[i] and array[j]
             // we use "destructuring assignment" syntax to achieve that
             // you'll find more details about that syntax in later chapters
             // same can be written as:
-            // let t = array[i];array[i] = array[j]; array[j] = t;
-
+            // let t = array[i];array[i] = array[j]; array[j] = t
             [array[i], array[j]] = [array[j], array[i]];
         }
 
-        console.log(array);
         return array;
-
     }
 
-    handleRandomise(array);
+    const handleRandomise = (e) => {
+        let players = ["Sven", "Mathilde", "Owen", "Bill"];
+        randomise(players);
+        console.log(players);
+    };
 
     return (
         <>
             <div className="card card-body col-md-6 mt-4">
                 <h4 className="card-title">The contenders are:</h4>
-                <p className="card-body">A list of the inputted names</p>
+                {/* <ul className="card-body list-group mt-4">
+                { players.map((name, i) => (
+                    <li className="list-group-item" key={ i }>{ name }</li>
+                )) }
+                </ul> */}
+
                 <button
                     className="btn btn-primary"
-                    // onClick={ handleRandomise }
+                    onClick={ handleRandomise }
                     >Generate random pairings
                 </button>
             </div>
--- src/components/PlayerForm.js
+++ src/components/PlayerForm.js
@@ -1,60 +1,45 @@
-import { Component } from "react";
+import { useState } from "react";
 
-class PlayerForm extends Component {
+const PlayerForm = () => {
 
-    constructor(props) {
-        super(props);
+    const [name, setName] = useState("");
+    const [players, setPlayers] = useState([]);
 
-        this.state = {
-            name: "",
-            players: [],
-        };
-
-        this.handleChange=this.handleChange.bind(this);
-        this.handleAdd=this.handleAdd.bind(this);
+    const handleChange = (e) => {
+        setName(e.currentTarget.value );
     }
 
-    handleChange(e) {
-        this.setState({ name: e.currentTarget.value });
-    }
-
-    handleAdd(e) {
+    const handleAdd = (e) => {
         e.preventDefault();
-        this.setState({
-            name: "",
-            // use of spread notation to create new array with existing array. Creates a new array rather than mutates the old one
-            players: [...this.state.players, this.state.name],
-        });
+        setPlayers([...players, name]);
+        setName("");
     }
 
-
-    render() {
-        return (
-            <>
-                <div className="card col-md-6 mt-4">
-                    <form className="card-body form-group">
-                        <h4 className="card-title">Add 4 players, one at a time</h4>
-                        <div className="form-inline mb-4">
-                            <label>Name</label>
-                            <input
-                                className="form-control ml-4"
-                                type="text"
-                                // placeholder="Harry"
-                                value={ this.name }
-                                onChange={ this.handleChange }
-                            />
-                        </div>
-                        <button
-                            className="btn btn-primary"
-                            onClick={ this.handleAdd }
-                            >Add player
-                        </button>
-                    </form>
-                </div>
-                
-            </>
-        )
-    }
+    return (
+        <>
+            <div className="card col-md-6 mt-4">
+                <form className="card-body form-group">
+                    <h4 className="card-title">Add 4 players, one at a time</h4>
+                    <div className="form-inline mb-4">
+                        <label>Name</label>
+                        <input
+                            className="form-control ml-4"
+                            type="text"
+                            placeholder="name"
+                            value={ name }
+                            onChange={ handleChange }
+                        />
+                    </div>
+                    <button
+                        className="btn btn-primary"
+                        onClick={ handleAdd }
+                        >Add player
+                    </button>
+                </form>
+            </div>
+            
+        </>
+    )
 
 
 }
