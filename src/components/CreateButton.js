569ab552422d1277b6efb0e06423e14d9f3a6f1e
Kate Fox <katefox100@gmail.com>
2021-04-01 15:23:45 +0100

    CreateButton updated to be functional rather than class based

--- src/components/CreateButton.js
+++ src/components/CreateButton.js
@@ -1,19 +1,17 @@
-import { Component } from "react";
+const CreateButton = ({ handleCreate, buttonText }) => {
 
-class CreateButton extends Component {
 
-    render() {
-        return (
-            <>
-                <button
-                    className="btn btn-primary mt-4 center"
-                    style={{ width: 100 + "%", fontSize: 1.5 + "rem"}}
-                    type="submit"
-                    onClick={ this.handleCreate }
-                >Start Tournament!</button>
-            </>
-        )
-    };
+
+    return (
+        <>
+            <button
+                className="btn btn-primary mt-4 center"
+                style={{ width: 100 + "%", fontSize: 1.5 + "rem"}}
+                type="submit"
+                onClick={ handleCreate }
+            >{ buttonText }</button>
+        </>
+    )
 
 }
 
