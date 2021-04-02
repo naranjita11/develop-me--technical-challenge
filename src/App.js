8ae53dd99fd2d7e1c1616d580e1ea4b930a55cfb
Kate Fox <katefox100@gmail.com>
2021-04-02 10:58:00 +0100

    react-router-dom installed plus history.js for routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
+import {
+  Router,
+  Route,
+  // Switch,
+} from "react-router-dom";
+import history from "./history";
 import Header from "../src/components/Header";
 import PlayerDisplay from "./components/PlayerDisplay";
+// import FourOhFour from './FourOhFour';
 
 function App() {
   return (
-    <>
-      <Header />
-      <PlayerDisplay />
-    </>
+    <Router history={ history }>
+      <>
+        {/* <Switch> */}
+          <Header />
+          <Route exact path="/">
+            <PlayerDisplay />
+          </Route>
+          <Route exact path="/started">
+
+          </Route>
+        {/* </Switch>
+        <FourOhFour /> */}
+      </>
+    </Router>
   );
 }
 
