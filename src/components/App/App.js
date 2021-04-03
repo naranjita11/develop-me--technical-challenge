7e601056d4912878149d43c460e21160332f8d9b
Kate Fox <katefox100@gmail.com>
2021-04-03 15:35:02 +0100

    wrapper setup and logicon App to display changing rounds

---
+++ src/components/App/App.js
@@ -0,0 +1,51 @@
+import "../../App.css";
+// import {
+  // Router,
+  // Route,
+  // Switch,
+// } from "react-router-dom";
+// import history from "./history";
+import Header from "../Header";
+import Settings from "../Settings";
+import QuarterFinals from "../QuarterFinals";
+import SemiAndFinals from "../SemiAndFinals";
+// import FourOhFour from './FourOhFour';
+
+function App({ stage }) {
+
+  let currentStage = <Settings />
+
+  if (stage === "quarterFinals") {
+    currentStage = <QuarterFinals />
+  };
+
+  if (stage === "semiAndFinals") {
+    currentStage = <SemiAndFinals />
+  };
+  
+  return (
+    <>
+      <Header />
+      { currentStage }
+    </>
+
+    // <Router history={ history }>
+    //   <>
+    //     <Switch>
+    //       <Header />
+    //       <Route exact path="/">
+    //         <PlayerDisplay />
+    //       </Route>
+    //       <Route exact path="/started">
+    //         <Round />
+    //       </Route>
+
+
+    //     </Switch>
+    //     <FourOhFour />
+    //   </>
+    // // </Router>
+  );
+}
+
+export default App;
