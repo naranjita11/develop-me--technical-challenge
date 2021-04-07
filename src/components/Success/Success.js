2a43e547752817736f52d3c9dd1051b6646f20e2
Kate Fox <katefox100@gmail.com>
2021-04-07 14:03:22 +0100

    success message animation complete

--- src/components/Success/Success.js
+++ src/components/Success/Success.js
@@ -1,20 +1,15 @@
 const Success = ({ winner }) => {
 
-    // const successStyling = {
-    //     backgroundColor: "gold",
-    // }
-     
     return (
     <div>
-        <header className="jumbotron mt-4 mb-4 text-center" style={{ backgroundColor: "gold" }}>
-            <h1>Well done, { winner }!</h1>
-        </header>
-        <header className="jumbotron mt-4 mb-4 text-center" style={{ backgroundColor: "#61dafb" }}>
-            <h1>Well done, { winner }!</h1>
-        </header>
-        <header className="jumbotron mt-4 mb-4 text-center" style={{ backgroundColor: "cornflowerblue" }}>
-            <h1>Well done, { winner }!</h1>
-        </header>
+        <div className="jumbotron mt-4 mb-4 text-center successBanner" style={{ color: "white"}}>
+            <h1 className="mb-4">Well done, { winner }!</h1>
+            <h2 className="mb-4">Well done, { winner }!</h2>
+            <h3 className="mb-4">Well done, { winner }!</h3>
+            <h4 className="mb-4">Well done, { winner }!</h4>
+            <h5 className="mb-4">Well done, { winner }!</h5>
+            <h6 className="mb-4">Well done, { winner }!</h6>
+        </div>
     </div>
     );
 }
